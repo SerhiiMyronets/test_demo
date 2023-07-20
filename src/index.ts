f0efbc78152fe456ee7af4e5e0698f5802456d86
SerhiiMyronets <hardmail88@gmail.com>
2023-07-20 16:50:53 -0700

    hi

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res ) => {
+
+    let helloWorld = 'Hello World!!1110011!!';
+
+    res.send(helloWorld)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
